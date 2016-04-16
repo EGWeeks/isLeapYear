84a5bd49054b1702423fc47a0de90f779874fa38
Ethan Weeks <ethanweeks@ymail.com>
2016-04-15 21:53:28 -0600

    first solution

--- leap.js
+++ leap.js
@@ -1,9 +1,20 @@
 'use strict';
 
-var year = {};
+var Year = function(year) {
+	this.year = year;
+};
 
-year.prototype.isLeap = = function(year) {
+Year.prototype.isLeap = function() {
+	var result;
 
-}
+	if(this.year % 4 === 0 && this.year % 100 !== 0) {
+		result = true;
+	} else if (this.year % 4 === 0 && this.year % 100 === 0 && this.year % 400 === 0) {
+		result = true;
+	} else {
+		result = false;
+	}
+	return result;
+};
 
-module.exports = year;
+module.exports = Year;
