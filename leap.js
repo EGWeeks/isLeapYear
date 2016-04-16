58012f61080463482135ebd6e9745843865f1cf1
Ethan Weeks <ethanweeks@ymail.com>
2016-04-15 22:08:43 -0600

    updated set initial result value to false + removed else statement

--- leap.js
+++ leap.js
@@ -5,14 +5,14 @@ var Year = function(year) {
 };
 
 Year.prototype.isLeap = function() {
-	var result;
+	
+	var result = false;
 
 	if(this.year % 4 === 0 && this.year % 100 !== 0 ||
 		this.year % 4 === 0 && this.year % 100 === 0 && this.year % 400 === 0) {
 		result = true;
-	} else {
-		result = false;
 	}
+
 	return result;
 };
 
